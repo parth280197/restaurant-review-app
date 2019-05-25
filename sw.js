e1648f7ce718712da814676353c6eaeb62a1626a
parth <parth280197@gmail.com>
2019-05-25 00:46:21 -0500

    Intial Commit

---
+++ sw.js
@@ -0,0 +1,59 @@
+const currentCache = "portfolio-cache-v1";
+const itemsToCache = [
+  'css/styles.css',
+  'data/restaurants.json',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg',
+  'js/dbhelper.js',
+  'js/main.js',
+  'js/restaurant_info.js',
+  'index.html',
+  'restaurant.html'
+];
+
+self.addEventListener('install', function(event) {
+  console.log('installed')
+  event.waitUntil(
+    caches.open(currentCache).then(function(cache) {
+      return cache.addAll(itemsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName != currentCache;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(currentCache).then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        }).catch(function(rej) {
+          console.log(rej);
+          console.log('rejected');
+        });
+      });
+    })
+  );
+});
